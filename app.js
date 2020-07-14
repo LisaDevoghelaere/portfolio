f3a2555fe4a22c7a2265559cfd47626c383166b3
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-07-14 16:13:40 +0200

    add mousemove & burger

---
+++ app.js
@@ -0,0 +1,25 @@
+const circle = document.querySelector('.circle');
+const burger = document.querySelector('.burger');
+const close = document.querySelector('nav>p>a');
+// console.log(circle);
+
+document.addEventListener('mousemove', e => {
+   circle.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+});
+
+//Burger et nav
+
+function toggleNavBar(){
+   const navBar = document.querySelector('nav');
+   navBar.classList.toggle('show');
+}
+// ajout de l'évènement
+burger.addEventListener('click', function(e){
+   e.preventDefault();
+   toggleNavBar();
+});
+// Ajout de l'évènement close
+close.addEventListener('click', function(e){
+   e.preventDefault();
+   toggleNavBar();
+});
