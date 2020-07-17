7c3a3c5d1e9b276098c7165afb350dfc6ce59e35
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-07-17 15:50:45 +0200

    section competences

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const burger = document.querySelector('.burger');
 const close = document.querySelector('nav>p>a');
 // console.log(circle);
 document.addEventListener('mousemove', function(e) {
-   circle.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+   circle.setAttribute('style', 'top:'+(e.pageY - 150)+"px; left:"+(e.pageX - 150)+"px;")
 });
 
 //Burger et nav
@@ -21,4 +21,18 @@ burger.addEventListener('click', function(e){
 close.addEventListener('click', function(e){
    e.preventDefault();
    toggleNavBar();
+});
+
+const btnWeb = document.querySelector('.btn-web');
+btnWeb.addEventListener('click', function(e){
+   e.preventDefault();
+   const competenceWeb = document.querySelector('.competence-web');
+   competenceWeb.classList.toggle('show-web');
+});
+
+const btnDesign = document.querySelector('.btn-design');
+btnDesign.addEventListener('click', function(e){
+   e.preventDefault();
+   const competenceDesign = document.querySelector('.competence-design');
+   competenceDesign.classList.toggle('show-design');
 });
